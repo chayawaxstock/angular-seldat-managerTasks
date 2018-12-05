2e379faef44f66b4608302f99fe4a70020f0040a
chayawaxstock <42530885+chayawaxstock@users.noreply.github.com>
2018-12-05 17:08:03 +0200

    עע

---
+++ finishcourseseldat/angular6 - php/src/app/shared/models/reportProject.ts
@@ -0,0 +1,17 @@
+import { Project } from "./project";
+import { DepartmentUser } from "./departmentUser";
+
+export class ReportProject{
+   id :number;
+    name :string;
+    customerName :string
+    dateBegin :Date;
+    dateEnd :Date;
+    totalHours :number;
+   sumHoursDo :number
+   precentsDone:number
+   daysleft :number
+   isFinish :boolean
+  teamLeader :string
+   items:ReportProject[]=[];
+}
