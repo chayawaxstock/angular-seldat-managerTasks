8540b3843e24eb50ce2c3838214c8573bb276735
chayawaxstock <42530885+chayawaxstock@users.noreply.github.com>
2018-12-05 17:07:43 +0200

    ככ

---
+++ angular-seldat-managerTasks/angular-manager-tasks/src/app/app.module.ts
@@ -0,0 +1,77 @@
+
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ManagerMenuComponent } from './manager-menu/manager-menu.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { UserService } from './shared/services/user.service';
+import { ChartsModule } from 'ng2-charts';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from "@angular/router";
+import { AuthGuard } from './shared/auth.guard';
+import { HttpModule } from '@angular/http';
+import { WorkerService } from './shared/services/worker.service';
+import { TeamleaderService } from './shared/services/teamleader.service';
+import { ManagerService } from './shared/services/manager.service';
+import { ExcelExportModule } from '@progress/kendo-angular-excel-export';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { HttpClient, HttpClientJsonpModule } from '@angular/common/http';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { GridEditFormComponent } from './edit-form/edit-form.component';
+import { EditService } from './shared/services/edit-service.service';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { TryComponent } from './try/try.component';
+
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    ManagerMenuComponent,
+    GridEditFormComponent,
+    TryComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    DateInputsModule,
+    DialogsModule,
+    GridModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpModule,
+   RouterModule, // Need this module for the routing
+   ChartsModule,
+   InputsModule,
+   ExcelExportModule,
+   HttpClientJsonpModule,
+   DialogModule,
+   DropDownsModule
+
+  ],
+  providers: [UserService,AuthGuard,WorkerService,TeamleaderService,ManagerService,  {
+    deps: [HttpClient],
+    provide: EditService,
+    useFactory: (jsonp: HttpClient) => () => new EditService(jsonp)
+}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
